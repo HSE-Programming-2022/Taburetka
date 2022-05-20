78eac8f954a6f5dcf73a469c165a890f3a04831a
knstntinovich <knstntinovich@DESKTOP-CMRA2FH>
2022-05-20 17:14:26 +0300

    Добавьте файлы проекта.

---
+++ TaburetkaProject/ToDo.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaburetkaProject
+{
+    /// <summary>
+    /// Логика взаимодействия для ToDo.xaml
+    /// </summary>
+    public partial class ToDo : Page
+    {
+        public ToDo()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void DragMove()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void lblNote_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textNote.Focus();
+        }
+
+        private void txtNote_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textNote.Text) && textNote.Text.Length > 0)
+            {
+                leblNote.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                leblNote.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void lblTime_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtTime.Focus();
+        }
+
+        private void txtTime_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTime.Text) && txtTime.Text.Length > 0)
+            {
+                lblTime.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lblTime.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
