798d7b67282bf76c4a5ebd3c9272bdc50513b690
shap1 <shap1@DESKTOP-86BHT2G>
2022-05-20 15:57:44 +0300

    Добавьте файлы проекта.

---
+++ WPFHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdminInterface
+{
+    public class WPFHelper
+    {
+        public static DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new("server=DESKTOP-86BHT2G\\SQLEXPRESS;Trusted_Connection=Yes;DataBase=QQMber;");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close();
+            return dataTable;
+        }
+
+        public static void Insert(string insertSQL)
+        {
+            SqlConnection sqlConnection = new("server=DESKTOP-86BHT2G\\SQLEXPRESS;Trusted_Connection=Yes;DataBase=QQMber;");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = insertSQL;                             // присваиваем команде текст
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+
+        }
+    }
+}
