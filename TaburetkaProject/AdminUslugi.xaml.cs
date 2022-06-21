eaf0b00d2252f7298db2c36872ef2947cec5ebb1
dariabalanuta <dariabalanuta@gmail.com>
2022-06-21 15:22:37 +0300

    dariab
    upd

---
+++ TaburetkaProject/AdminUslugi.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaburetkaProject
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminUslugi.xaml
+    /// </summary>
+    public partial class AdminUslugi : Window
+    {
+        public AdminUslugi()
+        {
+            InitializeComponent();
+        }
+    }
+}
