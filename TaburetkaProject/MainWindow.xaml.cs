4be6a4014a9494b85bed95f87c90ffc3b0065103
azubritskiy <105792443+azubritskiy@users.noreply.github.com>
2022-06-24 01:18:46 +0300

    X

---
+++ TaburetkaProject/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaburetkaProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new Dashboard();
+        }
+        private void TempOrders_Click(object sender, RoutedEventArgs e)
+
+        {
+            
+            MyFrame.Content = new TempOrders();
+        }
+        private void ToDo_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new ToDo();
+        }
+        private void Contracts_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new Contracts();
+        }
+        private void Notes_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Content = new Notes();
+        }
+
+        private void Clients_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MyFrame.Content = new Clients();
+
+        }
+    }
+}
---
+++ TaburetkaProject/Models/Storage.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaburetkaProject.Models
+{
+    internal class Storage
+    {
+        private const string filePath = "../../Data/ToDoList.json";
+
+        static public List<ToDoItem> items = new List<ToDoItem>();
+
+
+        public static void ReadItems()
+        {
+
+            string item = File.ReadAllText(filePath);
+            List<ToDoItem> itemsInfo = JsonConvert.DeserializeObject<List<ToDoItem>>(item);
+
+            if (itemsInfo != null) { items = itemsInfo; }
+            else { items = new List<ToDoItem>();}
+        }
+
+        public static void SaveItem(List<ToDoItem> items)
+        {
+            string itemsInfo = JsonConvert.SerializeObject(items, Formatting.Indented);
+            File.WriteAllText(filePath, itemsInfo);
+        }
+
+        public static void DeleteItem(ToDoItem item)
+        {
+            if (!string.IsNullOrEmpty(item.FileSource))
+            {
+                string folderFiles = "../../Data/Files/";
+                File.Delete(folderFiles + item.FileSource);
+            }
+            if (!string.IsNullOrEmpty(item.FileSource))
+            {
+                string folderImages = "../../Data/Images/";
+                File.Delete(folderImages + item.ImageSource);
+            }
+            items.Remove(item);
+            SaveItem(items);
+        }
+    }
+}
