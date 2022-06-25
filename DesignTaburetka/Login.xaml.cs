332374139416cd442f7620e48de3ac651c9bfb7c
Arseniy <belar2002@mail.ru>
2022-06-26 02:25:28 +0300

    Регистрация 1

--- DesignTaburetka/Login.xaml.cs
+++ DesignTaburetka/Login.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DesignTaburetka.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,22 +52,57 @@ namespace DesignTaburetka
             WindowState = WindowState.Minimized;
         }
         // End: Button Close | Restore | Minimize
-        
 
 
+        // Тут должно быть много елифов для каждого типа работника
         private void btnLogIn_Click(object sender, RoutedEventArgs e)
         {
-            if (loginText.Text.Length == 0 || password.Password.Length == 0)
+            var loginUser = loginText.Text;
+            var passwordUser = password.Password;
+
+            // Запрос для логина и пароля
+            string queryString1 = $"select emp_id, login, password from EmpData where login = '{loginUser}' and password = '{passwordUser}'";
+
+            // Запрос для департамента, имени, фамилии
+            string queryString2 = $"select b.emp_name, b.emp_surname, c.dep_name from EmpData a inner join Employee b on a.emp_id = b.emp_id inner join Department c on b.emp_dep_id = c.dep_id where login = '{loginUser}' and password = '{passwordUser}'";
+
+            // Запрос для должности
+            string queryString3 = $"select c.rank_name from EmpData a inner join Employee b on a.emp_id = b.emp_id inner join EmployeeRank c on b.rank_id = c.rank_id where login = '{loginUser}' and password = '{passwordUser}'";
+
+            // Запрос для имени
+            //string queryString4 = $"select b.emp_name, b.emp_surname from EmpData a inner join Employee b on a.emp_id = b.emp_id where login = '{loginUser}' and password = '{passwordUser}'";
+
+
+            DataTable table1 = WPFHelper.Select(queryString1);
+
+            DataTable table2 = WPFHelper.Select(queryString2);
+
+            DataTable table3 = WPFHelper.Select(queryString3);
+
+            //DataTable table4 = WPFHelper.Select(queryString4);
+
+
+            if (table1.Rows.Count == 1)
+            {
+                AdminUslugi adminUslugi = new AdminUslugi();
+                adminUslugi.UserFullName.Text = $"{table2.Rows[0]["emp_name"].ToString()}\n{table2.Rows[0]["emp_surname"].ToString()}";
+                adminUslugi.Department.Text = $"Отдел: {table2.Rows[0]["dep_name"].ToString()}";
+                adminUslugi.Position.Text = $"Должность: {table3.Rows[0]["rank_name"].ToString()}";
+                //adminUslugi.UserFullName.Text = $"{table4.Rows[0]["emp_name"].ToString()}\n{table4.Rows[0]["emp_surname"].ToString()}";
+                adminUslugi.Show();
+                this.Close();
+            }
+            else if (loginText.Text.Length == 0 || password.Password.Length == 0)
             {
                 MessageBox.Show("Вы не ввели все данные.");
                 loginText.Focus();
             }
-            else if (loginText.Text == "admin" && password.Password == "123456")
-            {
-                AdminUslugi _menuAdmin = new AdminUslugi();
-                _menuAdmin.Show();
-                this.Close();
-            }
+            //else if (loginText.Text == "admin" && password.Password == "123456")
+            //{
+            //    AdminUslugi _menuAdmin = new AdminUslugi();
+            //    _menuAdmin.Show();
+            //    this.Close();
+            //}
             else
             {
                 MessageBox.Show("Ошибка в веденных данных");
