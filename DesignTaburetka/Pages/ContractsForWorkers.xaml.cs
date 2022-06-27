b66777b8784601eb2eb69d0a82d2a7011780f474
Arseniy <belar2002@mail.ru>
2022-06-28 00:16:42 +0300

    Polovinka WorkerInterface i na vsykiy sluchai

---
+++ DesignTaburetka/Pages/ContractsForWorkers.xaml.cs
@@ -0,0 +1,68 @@
+﻿using DesignTaburetka.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesignTaburetka.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ContractsForWorkers.xaml
+    /// </summary>
+    public partial class ContractsForWorkers : Page
+    {
+        public ContractsForWorkers()
+        {
+            InitializeComponent();
+            DataTable DTInWorkData = WPFHelper.Select("SELECT a.work_id, a.came_at, a.work_start, a.suppose_days, a.fact_days, b.status_name, c.dep_name, d.emp_name, d.emp_surname, f.project_name " +
+                "FROM DepartmentWork a INNER JOIN OrderStatus b ON a.status_id = b.status_id " +
+                "INNER JOIN Department c ON a.dep_id = c.dep_id INNER JOIN Employee d ON a.emp_id = d.emp_id INNER JOIN OrderTask f ON a.order_id = f.order_id"
+                );
+            InWorkData.DataContext = DTInWorkData;
+
+            DataTable DTPlannedData = WPFHelper.Select("SELECT a.work_id, a.came_at, a.work_start, a.suppose_days, a.fact_days, b.status_name, c.dep_name, d.emp_name, d.emp_surname, f.project_name " +
+                "FROM DepartmentWork a INNER JOIN OrderStatus b ON a.status_id = b.status_id " +
+                "INNER JOIN Department c ON a.dep_id = c.dep_id INNER JOIN Employee d ON a.emp_id = d.emp_id INNER JOIN OrderTask f ON a.order_id = f.order_id"
+                );
+            PlannedData.DataContext = DTPlannedData;
+
+            DataTable DTCompletedData = WPFHelper.Select("SELECT a.work_id, a.came_at, a.work_start, a.suppose_days, a.fact_days, b.status_name, c.dep_name, d.emp_name, d.emp_surname, f.project_name " +
+                "FROM DepartmentWork a INNER JOIN OrderStatus b ON a.status_id = b.status_id " +
+                "INNER JOIN Department c ON a.dep_id = c.dep_id INNER JOIN Employee d ON a.emp_id = d.emp_id INNER JOIN OrderTask f ON a.order_id = f.order_id"
+                );
+            CompletedData.DataContext = DTCompletedData;
+
+            //DataTable DTOrderTask = WPFHelper.Select("SELECT a.order_id, a.projcet_name, c.client_name, a.created_at, a.suppose_days, " +
+            //                                         "a.comment, b.emp_name, b.emp_surname FROM OrderTask a inner join Employee b on a.added_by = b.emp_id inner join Client c on a.client_id = c.client_id");
+        }
+
+        private void BtnAddProject_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void BtnDeleteProject_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDeleteService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- DesignTaburetka/WorkerUslugi.xaml.cs
+++ DesignTaburetka/WorkerUslugi.xaml.cs
@@ -1,106 +1,91 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
-//using System.Windows.Controls.Primitives;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
-//namespace DesignTaburetka
-//{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-//    public partial class AdminUslugi : Window
-//    {
-        //public WorkerUslugi()
-        //{
-        //    InitializeComponent();
-        //    fContainer.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
-        //}
+namespace DesignTaburetka
+{
+    // <summary>
+    // Логика взаимодействия для MainWindow.xaml
+    // </summary>
+    public partial class WorkerUslugi : Window
+    {
+        public WorkerUslugi()
+        {
+            InitializeComponent();
+            // Я без понятия, что тут должно быть
+            fContainer2.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-        //private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    if (e.ChangedButton == MouseButton.Left)
-        //    {
-        //        this.DragMove();
-        //    }
-        //}
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
 
-        //private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    Tg_Btn.IsChecked = false;
-        //}
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
 
 
-        //// Start: Button Close | Restore | Minimize 
-        //private void btnClose_Click(object sender, RoutedEventArgs e)
-        //{
-        //    Close();
-        //}
+        // Start: Button Close | Restore | Minimize 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
 
-        //private void btnRestore_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (WindowState == WindowState.Normal)
-        //        WindowState = WindowState.Maximized;
-        //    else
-        //        WindowState = WindowState.Normal;
-        //}
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
 
-        //private void btnMinimize_Click(object sender, RoutedEventArgs e)
-        //{
-        //    WindowState = WindowState.Minimized;
-        //}
-        //// End: Button Close | Restore | Minimize
-        //private void btnHome_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    fContainer.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
-        //}
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        // End: Button Close | Restore | Minimize
+        private void btnHome_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer2.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-        //private void btnLogOut_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    Login _login = new Login();
-        //    _login.Show();
-        //    this.Close();
-        //}
+        private void btnLogOut_Checked(object sender, RoutedEventArgs e)
+        {
+            Login _login = new Login();
+            _login.Show();
+            this.Close();
+        }
 
-        //private void btnOrderTemp_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    fContainer.Navigate(new System.Uri("Pages/OrderTask.xaml", UriKind.RelativeOrAbsolute));
-        //}
+        private void btnContracts_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer2.Navigate(new System.Uri("Pages/Contracts.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-        //private void btnContracts_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    fContainer.Navigate(new System.Uri("Pages/Contracts.xaml", UriKind.RelativeOrAbsolute));
-        //}
+        private void btnToDo_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer2.Navigate(new System.Uri("Pages/ToDo.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-        //private void btnClients_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    fContainer.Navigate(new System.Uri("Pages/Clients.xaml", UriKind.RelativeOrAbsolute));
-
-        //}
-
-        //private void btnToDo_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    fContainer.Navigate(new System.Uri("Pages/ToDo.xaml", UriKind.RelativeOrAbsolute));
-        //}
-
-        //private void btnNotes_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    fContainer.Navigate(new System.Uri("Pages/Notes.xaml", UriKind.RelativeOrAbsolute));
-        //}
-
-        //private void btnDashboard_Checked(object sender, RoutedEventArgs e)
-        //{
-        //    fContainer.Navigate(new System.Uri("Pages/Dashboard.xaml", UriKind.RelativeOrAbsolute));
-        //}
-//    }
-//}
+        private void btnNotes_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer2.Navigate(new System.Uri("Pages/Notes.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
