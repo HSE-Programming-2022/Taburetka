6f54c1da49a150991e8eb1817720b7d91e27d429
Arseniy <belar2002@mail.ru>
2022-06-28 02:04:01 +0300

    ManagersUSlugi start

--- DesignTaburetka/ManagerUslugi.xaml.cs
+++ DesignTaburetka/ManagerUslugi.xaml.cs
@@ -6,101 +6,95 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
-//using System.Windows.Controls.Primitives;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
-//namespace DesignTaburetka
-//{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    //public partial class AdminUslugi : Window
-    //{
-    //    public AdminUslugi()
-    //    {
-    //        InitializeComponent();
-    //        fContainer.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
-    //    }
+namespace DesignTaburetka
+{
+    // <summary>
+    // Логика взаимодействия для MainWindow.xaml
+    // </summary>
+    public partial class ManagerUslugi : Window
+    {
+        public ManagerUslugi()
+        {
+            InitializeComponent();
+            fContainer3.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-    //    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-    //    {
-    //        if (e.ChangedButton == MouseButton.Left)
-    //        {
-    //            this.DragMove();
-    //        }
-    //    }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
 
-    //    private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    //    {
-    //        Tg_Btn.IsChecked = false;
-    //    }
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
 
 
-    //    // Start: Button Close | Restore | Minimize 
-    //    private void btnClose_Click(object sender, RoutedEventArgs e)
-    //    {
-    //        Close();
-    //    }
+        // Start: Button Close | Restore | Minimize 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
 
-    //    private void btnRestore_Click(object sender, RoutedEventArgs e)
-    //    {
-    //        if (WindowState == WindowState.Normal)
-    //            WindowState = WindowState.Maximized;
-    //        else
-    //            WindowState = WindowState.Normal;
-    //    }
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
 
-    //    private void btnMinimize_Click(object sender, RoutedEventArgs e)
-    //    {
-    //        WindowState = WindowState.Minimized;
-    //    }
-    //    // End: Button Close | Restore | Minimize
-    //    private void btnHome_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        fContainer.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
-    //    }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        // End: Button Close | Restore | Minimize
+        private void btnHome_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer3.Navigate(new System.Uri("Pages/MainPageUslugi.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-    //    private void btnLogOut_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        Login _login = new Login();
-    //        _login.Show();
-    //        this.Close();
-    //    }
+        private void btnLogOut_Checked(object sender, RoutedEventArgs e)
+        {
+            Login _login = new Login();
+            _login.Show();
+            this.Close();
+        }
 
-    //    private void btnOrderTemp_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        fContainer.Navigate(new System.Uri("Pages/OrderTask.xaml", UriKind.RelativeOrAbsolute));
-    //    }
+        private void btnOrderTemp_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer3.Navigate(new System.Uri("Pages/OrderTask.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-    //    private void btnContracts_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        fContainer.Navigate(new System.Uri("Pages/Contracts.xaml", UriKind.RelativeOrAbsolute));
-    //    }
+        private void btnContracts_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer3.Navigate(new System.Uri("Pages/Contracts.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-    //    private void btnClients_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        fContainer.Navigate(new System.Uri("Pages/Clients.xaml", UriKind.RelativeOrAbsolute));
+        private void btnToDo_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer3.Navigate(new System.Uri("Pages/ToDo.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-    //    }
+        private void btnNotes_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer3.Navigate(new System.Uri("Pages/Notes.xaml", UriKind.RelativeOrAbsolute));
+        }
 
-    //    private void btnToDo_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        fContainer.Navigate(new System.Uri("Pages/ToDo.xaml", UriKind.RelativeOrAbsolute));
-    //    }
-
-    //    private void btnNotes_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        fContainer.Navigate(new System.Uri("Pages/Notes.xaml", UriKind.RelativeOrAbsolute));
-    //    }
-
-    //    private void btnDashboard_Checked(object sender, RoutedEventArgs e)
-    //    {
-    //        fContainer.Navigate(new System.Uri("Pages/Dashboard.xaml", UriKind.RelativeOrAbsolute));
-    //    }
-    //}
-//}
+        private void btnTeam_Checked(object sender, RoutedEventArgs e)
+        {
+            fContainer3.Navigate(new System.Uri("Pages/Team.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
